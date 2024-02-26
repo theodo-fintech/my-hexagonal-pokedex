1bc0764225f7f136f0c5b9eb66f5a1256c5a190b
Dorian Frances <dfrances.pro@gmail.com>
2024-02-26 21:24:11 +0100

    chore: initial commit

---
+++ src/main/java/com/example/myhexagonalpokedex/MyHexagonalPokedexApplication.java
@@ -0,0 +1,13 @@
+package com.example.myhexagonalpokedex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHexagonalPokedexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyHexagonalPokedexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myhexagonalpokedex/MyHexagonalPokedexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myhexagonalpokedex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHexagonalPokedexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
