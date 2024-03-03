7d692ce06aa3adc16a50743791d9f132973998f1
Dorian Frances <dfrances.pro@gmail.com>
2024-03-03 20:12:27 +0100

    chore: add exceptions on postgres fetch call

--- src/main/java/com/example/myhexagonalpokedex/core/exception/ExceptionCode.java
+++ src/main/java/com/example/myhexagonalpokedex/core/exception/ExceptionCode.java
@@ -1,8 +1,9 @@
 package com.example.myhexagonalpokedex.core.exception;
 
 public enum ExceptionCode {
+    ERROR_WHILE_EXECUTING_HTTP_REQUEST(500),
     POKEAPI_ERROR(500),
-    ERROR_WHILE_EXECUTING_HTTP_REQUEST(500);
+    POSTGRES_FIND_POKEMON_ERROR(500);
 
     private final Integer httpStatusCode;
 
--- src/main/java/com/example/myhexagonalpokedex/infrastucture/postgresadapter/adapter/PostgresPokemonAdapter.java
+++ src/main/java/com/example/myhexagonalpokedex/infrastucture/postgresadapter/adapter/PostgresPokemonAdapter.java
@@ -1,5 +1,7 @@
 package com.example.myhexagonalpokedex.infrastucture.postgresadapter.adapter;
 
+import com.example.myhexagonalpokedex.core.exception.ExceptionCode;
+import com.example.myhexagonalpokedex.core.exception.MyHexagonalPokedexException;
 import com.example.myhexagonalpokedex.domain.pokemon.Pokemon;
 import com.example.myhexagonalpokedex.domain.pokemon.PokemonRepositoryFetcher;
 import com.example.myhexagonalpokedex.infrastucture.postgresadapter.mapper.PokemonEntityMapper;
@@ -18,6 +20,11 @@ public class PostgresPokemonAdapter implements PokemonRepositoryFetcher {
 
     @Override
     public List<Pokemon> findAll() {
-        return pokemonRepository.findAll().stream().map(PokemonEntityMapper::fromEntityToDomain).toList();
+        try {
+            return pokemonRepository.findAll().stream().map(PokemonEntityMapper::fromEntityToDomain).toList();
+        } catch (Exception e) {
+            final String message = "Error while fetching pokemons in database";
+            throw new MyHexagonalPokedexException(ExceptionCode.POSTGRES_FIND_POKEMON_ERROR, message, e);
+        }
     }
 }
