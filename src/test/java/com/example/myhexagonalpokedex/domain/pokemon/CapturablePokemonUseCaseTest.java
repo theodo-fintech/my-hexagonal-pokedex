804143abe299d694e0c8e579b61bd888dbaeb7f0
Dorian Frances <dfrances.pro@gmail.com>
2024-03-04 19:01:56 +0100

    test: first unit test

---
+++ src/test/java/com/example/myhexagonalpokedex/domain/pokemon/CapturablePokemonUseCaseTest.java
@@ -0,0 +1,56 @@
+package com.example.myhexagonalpokedex.domain.pokemon;
+
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class CapturablePokemonUseCaseTest {
+
+    @Mock
+    PokemonApiFetcher pokemonApiFetcher;
+
+    @Mock
+    PokemonRepositoryFetcher pokemonRepositoryFetcher;
+
+    @InjectMocks
+    CapturablePokemonUseCase capturablePokemonUseCase;
+
+
+    @Test
+    void should_return_capturable_pokemons() {
+        // Given
+        final List<Pokemon> mockOwnedPokemonList = List.of(
+                new Pokemon(1, "pokemon-1", "ability-1"),
+                new Pokemon(2, "pokemon-2", "ability-2"),
+                new Pokemon(4, "pokemon-4", "ability-4")
+        );
+        final List<CapturablePokemon> mockedCapturablePokemons = List.of(
+                new CapturablePokemon(1, "pokemon-1"),
+                new CapturablePokemon(2, "pokemon-2"),
+                new CapturablePokemon(3, "pokemon-3"),
+                new CapturablePokemon(4, "pokemon-4")
+        );
+        when(pokemonRepositoryFetcher.findAll()).thenReturn(mockOwnedPokemonList);
+        when(pokemonApiFetcher.findTopTwenty()).thenReturn(mockedCapturablePokemons);
+
+        // When
+        final List<CapturablePokemon> capturablePokemons = capturablePokemonUseCase.findAllInTopTwenty();
+
+        // Then
+        Assertions.assertThat(capturablePokemons)
+                .hasSize(1)
+                .containsExactlyInAnyOrder(
+                        new CapturablePokemon(
+                                3,
+                                "pokemon-3"
+                        )
+                );
+    }
+}
